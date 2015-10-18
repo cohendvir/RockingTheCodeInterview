6bf10efe1971f79094255f0fd3b423f0724caa85
Dvir <dvircohen100@gmail.com>
2015-10-18 13:16:18 +0300

    Question 1.5

---
+++ ArraysAndStrings/src/test/chapter1/Question1_5_Test.java
@@ -0,0 +1,23 @@
+package test.chapter1;
+
+import chapter1.Question1_5;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by Dvir on 18/10/2015.
+ */
+public class Question1_5_Test {
+
+    @Test
+    public void compressReturnNewString() {
+        String toCompress = "abbcccdddde";
+        Assert.assertEquals("a1b2c3d4e1", Question1_5.compress(toCompress));
+    }
+
+    @Test
+    public void compressReturnOldString() {
+        String toCompress = "abb";
+        Assert.assertEquals("abb", Question1_5.compress(toCompress));
+    }
+}
